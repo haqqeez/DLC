c30630b686f097175ddcb939ecf88142c9d8d4b8
haqqeez <60667080+haqqeez@users.noreply.github.com>
2022-04-03 23:42:47 -0400

    Update DLC_label.py

--- DLC_label.py
+++ DLC_label.py
@@ -1,11 +1,13 @@
 import deeplabcut
 import os
 
-# direction to the folder that containes all of the behavioural recordings in .avi format. Set to current working directory by default.
-my_videos_directory = os.getcwd() + '/'
+# direction to config.yaml for DLC algorithm you want to use for analysis
+# example: '/lustre03/project/rpp-markpb68/m3group/DLC/cozee_touchscreen-coco-2022-04-03/'
+path_config = '/absolute//path/to/config.yaml'
 
-#The following directory is the location of the config file of the DLC trained network that is going to be use for tracking. Current directory by default.
-path_config = my_videos_directory + '/' + 'config.yaml'
+# direction to the folder that containes all of the behavioural recordings in .avi format. Set to current working directory by default.
+# set to current working directory by deafult
+my_videos_directory = os.getcwd() + '/'
 
 print('-----LABELLING-----')
 
