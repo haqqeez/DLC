b3cd63dc27470710df746331796a6a606541068f
haqqeez <60667080+haqqeez@users.noreply.github.com>
2022-04-03 23:37:53 -0400

    Compatible with updated ParallelDLC script

--- DLC_traces.py
+++ DLC_traces.py
@@ -2,9 +2,11 @@ import deeplabcut
 import os
 import glob
 
-# direction to config.yaml for DLC algorithm you want to use for analysis
+# absolute path to config.yaml for DLC algorithm you want to use for analysis
 # example: '/lustre03/project/rpp-markpb68/m3group/DLC/cozee_touchscreen-coco-2022-04-03/'
-path_config = '/absolute//path/to/config.yaml'
+# leave as (capital letters) 'configpath' if using ParallelDLC.sh
+
+path_config = CONFIGPATH
 
 # direction to the folder that containes all of the behavioural recordings in .avi format. Set to current working directory by default.
 # set to current working directory by deafult
